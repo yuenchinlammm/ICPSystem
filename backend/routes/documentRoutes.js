3b3144413aa4649a6d38a8d031cfdf098f9fd96b
yuenchinlammm <yuenchinlammm124@gmail.com>
2025-08-17 11:39:08 +0000

    document upload functions added

---
+++ backend/routes/documentRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const requireAuth = require('../middlewares/auth');
+const validate = require('../middlewares/validate');
+const { idParamRule } = require('../validators/claim.validators');
+const { upload, handleUploadError } = require('../libs/upload');
+const { ownsClaim, ownsDocument } = require('../middlewares/ownership');
+const Claim = require('../models/Claim');
+const ctrl = require('../controllers/document.controller');
+
+router.use(requireAuth);
+
+// Upload to claim
+router.post('/claims/:id/documents',
+  idParamRule, validate, ownsClaim(Claim),
+  upload, handleUploadError,
+  ctrl.uploadDocument
+);
+
+// List claim docs
+router.get('/claims/:id/documents',
+  idParamRule, validate, ownsClaim(Claim),
+  ctrl.listDocuments
+);
+
+// Delete a doc
+router.delete('/documents/:docId',
+  ownsDocument(), // checks ownership
+  ctrl.deleteDocument
+);
+
+module.exports = router;
